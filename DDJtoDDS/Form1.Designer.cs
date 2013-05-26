231fd1a5b1ca68d1674ee9a1ced5e58c8ea4c1fe
Ryan <ryan@projecthax.com>
2013-05-26 16:51:20 -0400

    Initial commit

---
+++ DDJtoDDS/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace DDJtoDDS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenDDJFolder = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.OpenDDSFolder = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OpenDDJFolder
+            // 
+            this.OpenDDJFolder.Location = new System.Drawing.Point(6, 19);
+            this.OpenDDJFolder.Name = "OpenDDJFolder";
+            this.OpenDDJFolder.Size = new System.Drawing.Size(186, 23);
+            this.OpenDDJFolder.TabIndex = 0;
+            this.OpenDDJFolder.Text = "Convert To DDS (DirectDraw)";
+            this.OpenDDJFolder.UseVisualStyleBackColor = true;
+            this.OpenDDJFolder.Click += new System.EventHandler(this.OpenDDJFolder_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.OpenDDJFolder);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(198, 56);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DDJ to DDS";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.OpenDDSFolder);
+            this.groupBox2.Location = new System.Drawing.Point(12, 74);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(198, 56);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "DDS to DDJ";
+            // 
+            // OpenDDSFolder
+            // 
+            this.OpenDDSFolder.Location = new System.Drawing.Point(6, 19);
+            this.OpenDDSFolder.Name = "OpenDDSFolder";
+            this.OpenDDSFolder.Size = new System.Drawing.Size(186, 23);
+            this.OpenDDSFolder.TabIndex = 0;
+            this.OpenDDSFolder.Text = "Convert To DDJ (Joymax)";
+            this.OpenDDSFolder.UseVisualStyleBackColor = true;
+            this.OpenDDSFolder.Click += new System.EventHandler(this.OpenDDSFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 138);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "DDJ to DDS";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenDDJFolder;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button OpenDDSFolder;
+    }
+}
+
---
+++ DDJtoDDS/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace DDJtoDDS
+{
+    public partial class Form1 : Form
+    {
+        static String application_path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenDDJFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            DialogResult result = dlg.ShowDialog();
+
+            if (dlg.SelectedPath.Length > 0)
+            {
+                String[] files = Directory.GetFiles(dlg.SelectedPath, "*.ddj", SearchOption.TopDirectoryOnly);
+                if (files.Length > 0)
+                {
+                    MessageBox.Show("The conversion process may take several minutes depending on how many files there are. Afterwards the converted directory will pop up.", "DDJ to DDS", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    
+                    String converted_to_dds = application_path + "\\converted_to_dds\\";
+                    Directory.CreateDirectory(converted_to_dds);
+
+                    for (int x = 0; x < files.Length; ++x)
+                    {
+                        String dds_path = converted_to_dds + Path.GetFileNameWithoutExtension(files[x]) + ".dds";
+
+                        byte[] data = File.ReadAllBytes(files[x]);
+                        using (FileStream file = new FileStream(dds_path, FileMode.OpenOrCreate))
+                        {
+                            file.Write(data, 20, data.Length - 20);
+                            file.Close();
+                        }
+                    }
+
+                    Process.Start(converted_to_dds);
+                }
+                else
+                {
+                    MessageBox.Show("No DDJ's could be found in the specified directory.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void OpenDDSFolder_Click(object sender, EventArgs e)
+        {
+            // JMXVDDJ 1000........
+            byte[] ddj_header = { 0x4A, 0x4D, 0x58, 0x56, 0x44, 0x44, 0x4A, 0x20, 0x31, 0x30, 0x30, 0x30, 0x88, 0x80, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00 };
+
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            DialogResult result = dlg.ShowDialog();
+
+            if (dlg.SelectedPath.Length > 0)
+            {
+                String[] files = Directory.GetFiles(dlg.SelectedPath, "*.dds", SearchOption.TopDirectoryOnly);
+                if (files.Length > 0)
+                {
+                    MessageBox.Show("The conversion process may take several minutes depending on how many files there are. Afterwards the converted directory will pop up.", "DDJ to DDS", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                    String converted_to_ddj = application_path + "\\converted_to_ddj\\";
+                    Directory.CreateDirectory(converted_to_ddj);
+
+                    for (int x = 0; x < files.Length; ++x)
+                    {
+                        String ddj_path = converted_to_ddj + Path.GetFileNameWithoutExtension(files[x]) + ".ddj";
+
+                        byte[] data = File.ReadAllBytes(files[x]);
+                        using (FileStream file = new FileStream(ddj_path, FileMode.OpenOrCreate))
+                        {
+                            file.Write(ddj_header, 0, 20);
+                            file.Write(data, 0, data.Length);
+                            file.Close();
+                        }
+                    }
+
+                    Process.Start(converted_to_ddj);
+                }
+                else
+                {
+                    MessageBox.Show("No DDS's could be found in the specified directory.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
